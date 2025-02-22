972ca07dfa0ad58abc740eb7d77a4bd1dbfc117d
romamakar <roman.makarevych@gmail.com>
2025-02-22 16:34:52 +0200

    fix

--- tools/builder/ZipBuilder/HLocClass.cs
+++ tools/builder/ZipBuilder/HLocClass.cs
@@ -8,12 +8,11 @@ namespace ZipBuilder
 {
     public class HLocClass
     {
-        public string name { get; set; }
-        public byte? org_tbyte { get; set; }
-        public string value { get; set; }
         public List<HLocClass> children { get; set; }
-        public int numChildren { get; set; }
+        public string name { get; set; }
+        public int? numChildren { get; set; }
+        public byte? org_tbyte { get; set; }
         public string type { get; set; }
-
+        public string value { get; set; }
     }
 }
